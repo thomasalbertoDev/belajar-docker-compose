039d18de9f7d2e197dbdefbfe7d2b7549796a366
thomasalbertoDev <thomasalberto456@gmail.com>
2024-07-29 11:42:36 +0700

    first commit

---
+++ script.sh
@@ -0,0 +1,17 @@
+# Perintah ini digunakan untuk membuat container dengan docker compose
+docker compose create
+
+# Perintah ini digunakan untuk menjalankan container dengan docker compose
+docker compose start
+
+# Perintah ini digunakan untuk melihat container yang sedang berjalan di dalam configuration docker compose nya
+docker compose ps
+
+# Perintah ini digunakan untuk menghentikan container yang sedang berjalan di dalam configuration docker compose nya
+docker compose stop
+
+# Perintah ini digunakan untuk menghapus container yang sedang berjalan di dalam configuration docker compose nya
+docker compose down
+
+# Perintah ini digunakan untuk melihat project apa yang sedang di running di dalam docker compose nya 
+docker compose ls
