619e2a95a22cb019e66ee367b1242b4e2eee95a4
thomasalbertoDev <thomasalberto456@gmail.com>
2024-07-30 20:25:46 +0700

    Belajar volume

--- script.sh
+++ script.sh
@@ -15,3 +15,9 @@ docker compose down
 
 # Perintah ini digunakan untuk melihat project apa yang sedang di running di dalam docker compose nya 
 docker compose ls
+
+# Perintah ini digunakan untuk menghapus volume dengan nama tertentu
+docker volume rm {{ volume_name }}
+
+# Perintah ini digunakan untuk menghapus semua volume yang tidak terpakai
+docker volume prune
